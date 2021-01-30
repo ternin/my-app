0adbed5dfe29123a5262cad8ebdea43d5960b24b
ternin <78250324+ternin@users.noreply.github.com>
2021-01-30 13:53:11 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const Todo = ({Fname,Lname}) => {Fname = 'sudhir';
+Lname = 'Iyer'
+return (<h1>Hi </h1>)} 
+
+
+
+ReactDOM.render(<Todo/>, document.getElementById('root'))
+
